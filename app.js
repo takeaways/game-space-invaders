696f8d7146030f4894213a2b9ccd5e9a558916aa
JangGeonil <wkdrjsdlf2@gmail.com>
2020-12-08 22:41:48 +0900

    add Game Over situation

--- app.js
+++ app.js
@@ -63,4 +63,47 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   document.addEventListener("keydown", moveShooter);
+
+  const moveInvaders = () => {
+    const leftEdge = alienInvaders[0] % width === 0;
+    const rightEdge =
+      alienInvaders[alienInvaders.length - 1] % width === width - 1;
+
+    if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+      direction = width;
+    } else if (direction === width) {
+      if (leftEdge) direction = 1;
+      else direction = -1;
+    }
+
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      squares[alienInvaders[i]].classList.remove("invader");
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      alienInvaders[i] += direction;
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      squares[alienInvaders[i]].classList.add("invader");
+    }
+
+    if (squares[currentShooterIndex].classList.contains("invader", "shooter")) {
+      resultDisplay.textContent = "Game Over";
+      squares[currentShooterIndex].classList.add("boom");
+      clearInterval(invaderId);
+    }
+
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+      if (alienInvaders[i] > squares.length - (width - 1)) {
+        resultDisplay.textContent = "Game Over";
+        clearInterval(invader);
+      }
+    }
+  };
+
+  invader = setInterval(moveInvaders, 200);
+
+  const shoot = (e) => {
+    let laserId;
+    let currentLaserIndex = currentShooterIndex;
+  };
 }); //end
